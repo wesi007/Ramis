fc9c613ebe9fc2f77c5f1b854b46e12ccb4a0f86
Wesi007 <manqobawesi@gmail.com>
2024-05-20 18:26:12 +0200

    Finale

--- script.js
+++ script.js
@@ -10,7 +10,16 @@ $(window).on('load scroll',function(){
     $('.fa-bars').removeClass('fa-times');
     $('.navbar').removeClassClass('nav-tooggle');
 
+    if($(window).scrollTop() > 30){
+        $('.header').css({'background':'#e9091c','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)' });
+    }else{
+        $('.header').css({'background':'none','box-shadow':'none' });
+    }
+
 });
+
+
+
 
 
 
